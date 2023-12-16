3d2c2cb0952bf870bf1b5fae9e096e652290a8a6
Mike Mehl <michaelalanmehl@gmail.com>
2023-12-15 23:27:34 -0500

    My config.

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ VERSION = 0.9
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX = /home/mike/.local/bin
+MANPREFIX = /home/mike/.local/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
